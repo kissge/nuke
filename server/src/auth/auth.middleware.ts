cce152e9c4e602ea61ebca344da1fc8dfd62bc29
Yusuke Kido <yusuke.kido@legalscape.co.jp>
2018-09-23 11:05:47 +0000

    Admin API

--- server/src/auth/auth.middleware.ts
+++ server/src/auth/auth.middleware.ts
@@ -10,7 +10,11 @@ export class AuthMiddleware implements NestMiddleware {
   resolve(...args: any[]): MiddlewareFunction {
     return (req, res, next) => {
       if (req.user) {
-        next();
+        if (req.path.substr(0, 6) === '/admin' && !req.user.isAdmin) {
+          throw new HttpException('You do not have privilege', 403);
+        } else {
+          next();
+        }
       } else {
         throw new HttpException('Login required', 401);
       }
